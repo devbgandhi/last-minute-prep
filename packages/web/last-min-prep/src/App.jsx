60bed6b6ecd2ae74d6f50ab1f985ba5480a4cb96
devbgandhi <devgandhi1804@gmail.com>
2026-07-06 06:40:37 -0700

    react app for the frontend

---
+++ packages/web/last-min-prep/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      // Find the API URL in the output of the `npx sst dev` command and paste it below
+      <h1>Last Minute Prep</h1>
+    // <p>Upload your resume to get started!</p>
+    // Make a POST request to the /upload-resume endpoint with the file name and user ID in the request body. The response will contain a signed URL for uploading the resume to S3. 
+    
+
+
+
+    </>
+  )
+}
+
+export default App
